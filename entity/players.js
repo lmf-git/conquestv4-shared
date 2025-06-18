21d5df2460018261781d95327504bb84518cdf5a
lmf-git <contact@dreamsbyte.com>
2025-06-18 04:23:21 -0300

    improved car movement

--- entity/players.js
+++ entity/players.js
@@ -1,8 +1,8 @@
 export class PlayerConstants {
   static RADIUS = 0.3;
-  static HEIGHT = 1.8; // Total height including radius caps
+  static HEIGHT = 1.8; // Total height of capsule
   static SPEED = 5.0;
-  static JUMP_FORCE = 8.0;
+  static JUMP_FORCE = 6.0; // Adjusted for better feel
   static MOUSE_SENSITIVITY = 0.002;
   static MAX_HEALTH = 100;
 }
--- entity/vehicles.js
+++ entity/vehicles.js
@@ -1,10 +1,16 @@
-export class VehicleConstants {
-  static CAR_SPEED = 20.0;  // Increased from 15.0
-  static CAR_TURN_SPEED = 1.5;  // Decreased from 2.0 for smoother turning
-  static CAR_SIZE = { width: 2, height: 1, length: 4 };
-  static INTERACTION_RANGE = 3.0;
-}
+export const VehicleTypes = {
+  CAR: 'car',
+  TRUCK: 'truck',
+  TANK: 'tank'
+};
 
-export class VehicleTypes {
-  static CAR = 'car';
+export class VehicleConstants {
+  static INTERACTION_RANGE = 5;
+  static CAR_SIZE = {
+    width: 2,
+    height: 1.2,
+    length: 4
+  };
+  static CAR_SPEED = 15;
+  static CAR_TURN_SPEED = 2;
 }
