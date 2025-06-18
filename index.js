0c4723af2eee64521c66b174373b81c01bf5a274
lmf-git <contact@dreamsbyte.com>
2025-06-17 23:15:10 -0300

    simplifying

--- index.js
+++ index.js
@@ -7,14 +7,3 @@ export * from './entity/weapons.js';
 export const GameConstants = {
   TICK_RATE: 60
 };
-
-// Helper for vector interpolation
-export const Vector = {
-  lerpVector3(a, b, t) {
-    return {
-      x: a.x + (b.x - a.x) * t,
-      y: a.y + (b.y - a.y) * t,
-      z: a.z + (b.z - a.z) * t
-    };
-  }
-};
